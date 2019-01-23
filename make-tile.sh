6f52c7a36082ee7ea91d435a4d5700dc768bad01
David Brooks <dave@bcs.co.nz>
2019-01-24 12:41:16 +1300

    Don't fuzzy compare colours when detecting transparency.

--- make-tile.sh
+++ make-tile.sh
@@ -4,7 +4,7 @@ rm -f ./$1/tiles/tile.png
 rm -f ./$1/tiles/alpha.png
 #
 svgexport ./$1/images/$2.svg ./$1/tiles/tile.png 200:0:400:720 25x
-convert ./$1/tiles/tile.png -fuzz 5% -transparent white ./$1/tiles/alpha.png
+convert ./$1/tiles/tile.png -transparent white ./$1/tiles/alpha.png
 ~/build/flatmaps/gdal2tiles.py -p raster -w none -r average -t $1 -x ./$1/tiles/alpha.png ./$1/tiles/$2/
 #
 rm -f ./$1/tiles/tile.png
