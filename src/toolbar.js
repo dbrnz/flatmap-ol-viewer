30985832978fd3641b0eeceb0ece98db7b5bf878
David Brooks <dave@bcs.co.nz>
2019-03-21 14:59:56 +1300

    Bundle FontAwesome fonts with our web application.

--- src/toolbar.js
+++ src/toolbar.js
@@ -22,6 +22,12 @@ limitations under the License.
 
 //==============================================================================
 
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/regular.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+
+//==============================================================================
+
 import Control from 'ol/control/Control';
 
 //==============================================================================
